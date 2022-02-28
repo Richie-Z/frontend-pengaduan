32e9ff2fabe60d053be25ac53d9b3adf65f11888
Richie-Z <zakariarichie@yahoo.com>
2022-02-28 18:43:30 +0700

    sidebar color to taliwind config & link active

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,20 @@ module.exports = {
       fontFamily: {
         karla: ["Karla"],
       },
+      colors: {
+        sidebar: {
+          50: "#6f9aff",
+          100: "#6590ff",
+          200: "#5b86ff",
+          300: "#517cff",
+          400: "#4772ff",
+          500: "#3d68ff",
+          600: "#335ef5",
+          700: "#2954eb",
+          800: "#1f4ae1",
+          900: "#1540d7",
+        },
+      },
     },
   },
   plugins: [],
