6aee63c8e599d11f4ecb82badea5be4864b2ab11
Richie-Z <zakariarichie@yahoo.com>
2022-02-28 17:17:00 +0700

    add fontawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,10 +12,13 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@/assets/css/main.css"],
+  css: [
+    "@/assets/css/main.css",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["@/plugins/axios"],
+  plugins: ["@/plugins/axios", "@/plugins/fontawesome.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
