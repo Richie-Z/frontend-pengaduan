3500d8d4e2acaa67e379fcb28ae1219d64ea162c
Richie-Z <zakariarichie@yahoo.com>
2022-03-08 15:54:21 +0700

    add vue good table

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,11 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["@/plugins/axios", "@/plugins/fontawesome.js"],
+  plugins: [
+    "@/plugins/axios",
+    "@/plugins/fontawesome.js",
+    "@/plugins/vuetable.client.js",
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
