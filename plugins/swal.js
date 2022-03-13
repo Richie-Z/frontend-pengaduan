02645ba9afab84711fe5b15e975b3d53f7e231e4
Richie-Z <zakariarichie@yahoo.com>
2022-03-13 22:23:45 +0700

    change timer

--- plugins/swal.js
+++ plugins/swal.js
@@ -4,7 +4,7 @@ const Toast = Swal.mixin({
   toast: true,
   position: "top-end",
   showConfirmButton: false,
-  timer: 3000,
+  timer: 1500,
   timerProgressBar: true,
   didOpen: (toast) => {
     toast.addEventListener("mouseenter", Swal.stopTimer);
