4e7aa6d118b439c1fd17c3c4359369d165fed7b1
Richie-Z <zakariarichie@yahoo.com>
2022-03-19 23:35:15 +0700

    class ordering and change lorem text

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,21 +1,19 @@
 <template>
   <div
-    class="m-auto text-stone-300 w-full shadow-xl"
+    class="m-auto w-full text-stone-300 shadow-xl"
     :class="[
       {
         'p-44 text-stone-300': isIndex,
         'p-20 text-stone-100': !isIndex && !isAdd,
-        'py-1 bg-stone-900': isAdd,
+        'bg-stone-900 py-1': isAdd,
       },
       isIndex ? 'bg-stone-900' : status,
     ]"
   >
     <div class="container mx-auto text-center">
-      <h1 class="text-7xl font-semibold mb-10">{{ title }}</h1>
-      <p class="text-xl leading-relaxed px-20" v-if="isIndex">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, et
-        fugiat dolorum dolores est ex magnam repellat quos id cum quidem
-        pariatur aperiam, ducimus accusantium at, sequi ut dicta ratione?
+      <h1 class="mb-10 text-7xl font-semibold">{{ title }}</h1>
+      <p class="px-20 text-xl leading-relaxed" v-if="isIndex">
+        Tempat Untuk Mengadu Semua Yang Mau Diadukan
       </p>
     </div>
   </div>
